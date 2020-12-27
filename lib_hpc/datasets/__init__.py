108c0d05f93111d5d6550ebd067be482e16f2aad
Siddharth Shrivastava <sid97.cstaff@klogin01.hpc.iitd.ac.in>
2020-12-27 11:33:55 +0530

    hpc

---
+++ lib_hpc/datasets/__init__.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Ke Sun (sunk@mail.ustc.edu.cn)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .cityscapes import Cityscapes as cityscapes
+from .lip import LIP as lip
+from .pascal_ctx import PASCALContext as pascal_ctx
+from .dark_zurich import Dark_Zurich as dark_zurich
+from .dark_zurich_enhanced import Dark_Zurich_Enhanced as dark_zurich_enhanced
+from .fda import FDA as fda
